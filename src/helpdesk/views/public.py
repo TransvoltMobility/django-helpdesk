15f0a96ecd631b3f238ac68891e612eb3efc62a5
shruthiamu14 <shruthiammu470@gmail.com>
2025-09-18 19:28:58 +0530

    [modified] css

--- src/helpdesk/views/public.py
+++ src/helpdesk/views/public.py
@@ -51,6 +51,11 @@ class UnifiedCreateTicketView(abstract_views.AbstractCreateTicketMixin, FormView
     template_name = "helpdesk/create_ticket.html"
     form_class = TicketForm
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['hide_ticket_button'] = True
+        return context
+
     def get_initial(self):
         initial_data = super().get_initial()
         return initial_data
@@ -191,6 +196,11 @@ class SuccessIframeView(TemplateView):
 
 class CreateTicketView(BaseCreateTicketView):
     template_name = "helpdesk/public_create_ticket.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['hide_ticket_button'] = True
+        return context
 
     def get_form(self, form_class=None):
         form = super().get_form(form_class)
